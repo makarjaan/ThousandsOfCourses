3b3f5e38b35459a08cafbe7cc349aa19fa6204de
makarjaan <arina.makarova.08@inbox.ru>
2025-11-01 17:24:15 +0300

    build: add hilt to app and activity

---
+++ app/src/main/java/makarova/thousandsofcourses/App.kt
@@ -0,0 +1,12 @@
+package makarova.thousandsofcourses
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/makarova/thousandsofcourses/MainActivity.kt
@@ -0,0 +1,15 @@
+package makarova.thousandsofcourses
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.enableEdgeToEdge
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+    }
+}
