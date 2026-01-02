21b6f118eb196a7913f5552905677764f5583f57
makarjaan <arina.makarova.08@inbox.ru>
2026-01-02 15:36:03 +0300

    build: remove unnecessary features, replace the project theme

--- feature/favorite/api/src/main/java/makarova/thousandsofcourses/feature/favorite/api/repository/FavoriteRepository.kt
+++
@@ -1,8 +0,0 @@
-package makarova.thousandsofcourses.feature.favorite.api.repository
-
-import makarova.thousandsofcourses.feature.main.api.model.CourseModel
-
-interface FavoriteRepository {
-
-    suspend fun getFavoriteCourses(): List<CourseModel>
-}
